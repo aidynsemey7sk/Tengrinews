59b9af2570c64800a3b3eef8c289d1dc8c734e37
aidyn <aidynsemey7sk@gmail.com>
2022-05-21 00:12:36 +0600

    deploy

--- news/admin.py
+++ news/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Category, Post, PostImage, Tag, Comment
+from .models import Category, Post, PostImage, Tag
 
 admin.site.register(Comment)
 admin.site.register(Tag)
