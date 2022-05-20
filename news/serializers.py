b36e0d613fe55347e433006226b72b94a6340068
aidyn <aidynsemey7sk@gmail.com>
2022-05-21 00:09:01 +0600

    deploy

--- news/serializers.py
+++ news/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from .models import Category, Post, PostImage, Comment
+from .models import Category, Post, PostImage
 from django.contrib.auth.models import User
 
 
