87293892ad62cd238f838b4642a484f69cc08ba3
aidyn <aidynsemey7sk@gmail.com>
2022-05-21 13:39:17 +0600

    deploy

--- tengrinews/settings/production.py
+++ tengrinews/settings/production.py
@@ -68,7 +68,7 @@ ROOT_URLCONF = 'tengrinews.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'staticfiles')],
+        'DIRS': [],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
