96f1c0a74af0e142378e41f53607b740dec9d7c4
aidyn <aidynsemey7sk@gmail.com>
2022-05-21 16:37:00 +0600

    deploy

--- tengrinews/settings/production.py
+++ tengrinews/settings/production.py
@@ -48,6 +48,7 @@ INSTALLED_APPS = [
 
     'news',
 
+    'storages',
     "corsheaders",
     'rest_framework',
 ]
@@ -140,21 +141,14 @@ USE_TZ = False
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-# # Add static folder to STATIC_DIRS
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
-]
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
-STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 
-# Default primary key field type
-# https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
+
+
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
