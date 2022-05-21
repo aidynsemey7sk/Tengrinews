c7b9d873fb1faf772612cf4674906b25c6280f19
aidyn <aidynsemey7sk@gmail.com>
2022-05-21 12:15:09 +0600

    deploy

--- tengrinews/settings/production.py
+++ tengrinews/settings/production.py
@@ -139,7 +139,7 @@ USE_TZ = False
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/uploads/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles') #this is not used
 # Add static folder to STATIC_DIRS
 STATICFILES_DIRS = [
