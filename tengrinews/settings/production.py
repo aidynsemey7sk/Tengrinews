cfd80776ead7e816e2a45cc58f031fe24de99095
aidyn <aidynsemey7sk@gmail.com>
2022-05-21 10:23:45 +0600

    deploy

--- tengrinews/settings/production.py
+++ tengrinews/settings/production.py
@@ -141,6 +141,9 @@ USE_TZ = False
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'images')
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
@@ -151,4 +154,4 @@ REST_FRAMEWORK = {
     'DATETIME_FORMAT': "%d.%b.%Y %H:%M",
 }
 
-STATIC_ROOT = BASE_DIR.joinpath('staticfiles')
+# STATIC_ROOT = BASE_DIR.joinpath('staticfiles')
