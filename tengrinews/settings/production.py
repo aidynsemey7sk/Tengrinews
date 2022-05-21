2a6414491d2bb246dd52769e909f0a04661ba4be
aidyn <aidynsemey7sk@gmail.com>
2022-05-21 11:27:21 +0600

    deploy

--- tengrinews/settings/production.py
+++ tengrinews/settings/production.py
@@ -145,10 +145,11 @@ STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR.joinpath('staticfiles')
 MEDIA_ROOT = os.path.join(BASE_DIR, 'uploads')
 
-# STATICFILES_DIRS = (
-#     os.path.join(BASE_DIR, 'uploads'),
-# )
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
