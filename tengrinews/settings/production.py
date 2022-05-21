2119ea689a6b4cb0b4dccace10633e9e1de970b2
aidyn <aidynsemey7sk@gmail.com>
2022-05-21 14:03:14 +0600

    deploy

--- tengrinews/settings/production.py
+++ tengrinews/settings/production.py
@@ -142,8 +142,8 @@ USE_TZ = False
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-# MEDIA_URL = '/media/'
-# MEDIA_ROOT = os.path.join(BASE_DIR, 'news')
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # # Add static folder to STATIC_DIRS
 # STATICFILES_DIRS = [
 #     os.path.join(BASE_DIR, 'static'),
